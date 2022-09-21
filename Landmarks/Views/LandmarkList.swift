bcd6db01ba66e3036c44da19b79753fa7e01b1bb
seo matsushita <seo.matsushita@kana-me.com>
2022-09-21 22:34:30 +0900

    Section 5 「Make the List Dynamic」まで完了し、コミット

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 松下世緒 on 2022/09/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
