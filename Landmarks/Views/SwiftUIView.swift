85fae814a0ce875c97a9c070ad1b0b90848016ea
seo matsushita <seo.matsushita@kana-me.com>
2022-09-21 22:56:58 +0900

    Section 6 「Set Up Navigation Between List and Detail」までコミット

---
+++ Landmarks/Views/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Landmarks
+//
+//  Created by 松下世緒 on 2022/09/21.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
