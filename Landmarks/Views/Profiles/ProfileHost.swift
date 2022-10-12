dcce7a7d1cffce92669990bb53d764b1b0832029
seo matsushita <seo.matsushita@kana-me.com>
2022-10-13 00:03:58 +0900

    Step 5 Section 2 Add an Edit Mode まで完了のため、コミット

--- Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
     @State private var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummary(profile: draftProfile)
+            HStack{
+                Spacer()
+                EditButton()
+            }
+            ProfileSummary(profile: modelData.profile)
         }
         .padding()
     }
@@ -21,5 +27,6 @@ struct ProfileHost: View {
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
