edd32b84d9d74974d87ee4ff7615a6f56128ae33
seo matsushita <seo.matsushita@kana-me.com>
2022-10-12 20:38:55 +0900

    Section 1 Display a User Profile まで完了のため、コミット

--- Landmarks/Views/Profiles/ProfileSummary.swift
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ProfileSummary: View {
+    @EnvironmentObject var modelData: ModelData
     var profile: Profile
     
     
@@ -22,6 +23,37 @@ struct ProfileSummary: View {
                 Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
                 Text("Goal Date:") + Text(profile.goalDate, style: .date)
                 
+                
+                Divider()
+                
+                VStack (alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+                    
+                    ScrollView(.horizontal) {
+                        
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 90))
+                            
+                            HikeBadge(name: "Tenth Hike")
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+                    
+                    HikeView(hike: modelData.hikes[0])
+                }
+                
             }
         }
     }
@@ -30,5 +62,6 @@ struct ProfileSummary: View {
 struct ProfileSummary_Previews: PreviewProvider {
     static var previews: some View {
         ProfileSummary(profile: Profile.default)
+            .environmentObject(ModelData())
     }
 }
